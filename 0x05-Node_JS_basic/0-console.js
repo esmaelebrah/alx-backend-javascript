3ccda22b1dc4719f08e29c09495de1040030aa20
esmaelebrah <esmaelebrahimhasengmail.com>
2024-06-30 15:00:54 +0300

    updated

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-const displayMessage = function(str) {
-	console.log(str);
-}
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const displayMessage = (str) => {
+  console.log(str);
+};
 
 module.exports = displayMessage;
